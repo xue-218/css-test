a88dd58b9c6274c4cd9cf9692d87a3a21384fb6c
xuekeai <xuetingxx@outlook.com>
2021-05-03 10:08:00 +0800

    初版提交

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    datalist:[
+      {title:'抗议卫生科普',text:'不局限与本次新冠肺炎，可以以增加非典，禽流感等，可取传染病防治知识，提高全名卫生保健意识。',url:'../../images/s-science.png'},
+      {title:'新馆肺炎抗疫故事',text:'记录本次疫情期间老百姓亲身经历的故事，颗考虑做剧情向游戏，互动电影游戏。',url:'../../images/s-story.png'},
+      {title:'致敬白衣天使',text:'通过游戏的方式，让大众了解义务工作者在本次抗议作战中的伟大贡献：与时间赛跑与病毒顽强作战，集体主义奉献精神等......颗考虑做策略类，动作类的游戏。',url:'../../images/s-angel.png'}
+    ],
+    speciallyData:[
+      {url:'../../images/process-1.svg',text:'6月10日\n 投稿开始'},
+      {url:'../../images/process-2.svg',text:'6月10日\n 入围公布'},
+      {url:'../../images/process-3.svg',text:'6月30日-9月30日\n 制作周期'},
+      {url:'../../images/process-4.svg',text:'6月10日\n 投放周期'},
+    ]
+  },
+ 
+ 
+ 
+})
